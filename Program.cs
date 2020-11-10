58a91b50b53febcb61e80a98d37281cf2dc59a9e
IlyaIIS <ilya1221221@mail.ru>
2020-11-10 17:23:09 +0500

    V0.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Feelwords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
