ed91a510e59d79c8db0187e2076a97fda9bb1e54
IlyaIIS <ilya1221221@mail.ru>
2020-11-13 22:29:16 +0500

    V0.5.0 Добавлен класс работы со словами

---
+++ FileWorker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Fillwords
+{
+    static class DataWorker
+    {
+        static public string[] ReadWordsFromFile(string path)
+        {
+            var text = File.ReadLines(path);
+
+            string[] output = new string[text.Count()];
+
+            for (int i = 0; i < output.Length; i++) output[i] = text.Skip(i).First();
+
+            return output;
+        }
+    }
+}
