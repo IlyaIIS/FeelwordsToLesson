4d0aecf4fda5b8076cdacb3219b468225eaaffcc
IlyaIIS <ilya1221221@mail.ru>
2020-11-14 10:07:12 +0500

    V0.5.1 Добавлен класс-хранилище для слов

--- FileWorker.cs
+++ FileWorker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -17,4 +18,32 @@ namespace Fillwords
             return output;
         }
     }
+
+    struct WordsSet
+    {
+
+        public string[] allWords;            //Массив из всех слов
+        public List<List<string>> setWords;  //Массив массивов слов, сгрупперованных по длине
+        public WordsSet(string[] input) 
+        {
+            this.allWords = input;
+            this.setWords = new List<List<string>>();
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i].Length > this.setWords.Count)
+                    {
+                        do
+                        {
+                            this.setWords.Add(new List<string>());
+                        } while (this.setWords.Count < input[i].Length);
+                    }
+
+                    this.setWords[input[i].Length].Add(input[i]);
+                }
+            }
+        }
+
+    }
+
 }
